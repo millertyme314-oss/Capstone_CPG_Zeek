108e252c3c278fff4c8595ccded204bb1d0c198b
Zeek Millz <228266305+millertyme314-oss@users.noreply.github.com>
2026-06-13 18:53:40 -0400

    Py files edit

--- Lab1/Attend_CPG.py
+++ Lab1/Attend_CPG.py
@@ -8,8 +8,8 @@ Theo_Univ = "Theo University is in session..."
 
 
 if In_Class >= 1 and Replay >= 1:
-    print(Theo_Univ +", Welcome to the Blue Ocean")
+    print(Theo_Univ +"Welcome to the Blue Ocean")
 elif In_Class < 1 and Replay >= 1:
-    print(Theo_Univ,"Gynocracy awaits; if you don't shape up!")
+    print(Theo_Univ+"Gynocracy awaits; if you don't shape up!")
 else: 
-    print(Theo_Univ,"Piss Off; Keisha awaits in Red Ocean!!")
+    print(Theo_Univ+"Piss Off; Keisha awaits in Red Ocean!!")
